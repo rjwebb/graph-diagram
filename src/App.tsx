3e3e192793ae5f219c506668128c75941b88764b
Bob Webb <bob.wbb@gmail.com>
2025-01-20 14:37:22 +0100

    add button to copy/paste svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { useRef } from "react";
 import "./App.css";
 import { Graph } from "./Graph";
 import { step7 } from "./steps";
 
 function App() {
-  return <Graph step={step7} />;
+  const svgRef = useRef(null);
+
+  const copyToClipboard = async () => {
+    if (!svgRef.current) return;
+    const svgContent = (svgRef.current as any).outerHTML;
+    await navigator.clipboard.writeText(svgContent);
+  };
+
+  return (
+    <>
+      <svg
+        ref={svgRef}
+        xmlns="http://www.w3.org/2000/svg"
+        width="565px"
+        height="310px"
+        style={{ border: "solid black 1px" }}
+      >
+        <Graph step={step7} />
+      </svg>
+      <br />
+      <button onClick={copyToClipboard}>...</button>
+    </>
+  );
 }
 
 export default App;
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -17,105 +17,98 @@ export const Graph = ({ step }: { step: Step }) => {
 
   return (
     <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="565px"
-        height="310px"
-        style={{ border: "solid black 1px" }}
-      >
-        {nodes.map((node, i) => {
-          const x = startX + node.x * nodeXGap;
-          const y = startY + node.y * nodeYGap;
-          return (
-            <>
-              {node.label === highlightedNode && (
-                <rect
-                  key={`highlight-${i}`}
-                  x={x - 35}
-                  y={y - 70}
-                  fill="lightgreen"
-                  width="80px"
-                  height="100px"
-                  rx="20px"
-                />
-              )}
-              <circle
-                key={`circle-${i}`}
-                cx={x}
-                cy={y}
-                r={nodeRadius}
-                fill="white"
-                stroke="black"
-                strokeWidth="3"
+      {nodes.map((node, i) => {
+        const x = startX + node.x * nodeXGap;
+        const y = startY + node.y * nodeYGap;
+        return (
+          <>
+            {node.label === highlightedNode && (
+              <rect
+                key={`highlight-${i}`}
+                x={x - 35}
+                y={y - 70}
+                fill="lightgreen"
+                width="80px"
+                height="100px"
+                rx="20px"
               />
-
-              <text
-                key={`label-${i}`}
-                x={x}
-                y={y}
-                fontFamily="Arial, sans-serif"
-                fontSize="22"
-                text-anchor="middle"
-                dominant-baseline="central"
-                fill="black"
-              >
-                {node.label}
-              </text>
-
-              <text
-                key={`branch-${i}`}
-                x={x + tagOffsetX}
-                y={y - tagOffsetY}
-                fontFamily="Arial, sans-serif"
-                fontSize="14"
-                fill="black"
-              >
-                branch: {node.branch}
-              </text>
-              <text
-                key={`clock-${i}`}
-                x={x + tagOffsetX}
-                y={y - tagOffsetY - tagSpacingY}
-                fontFamily="Arial, sans-serif"
-                fontSize="14"
-                fill="black"
-              >
-                clock: {node.clock}
-              </text>
-            </>
-          );
-        })}
-
-        {edges.map(({ from, to }) => {
-          const fromNode = nodesByLabel[from];
-          const toNode = nodesByLabel[to];
-
-          const x1 = startX + fromNode.x * nodeXGap;
-          const y1 = startY + fromNode.y * nodeYGap;
-
-          const x2 = startX + toNode.x * nodeXGap;
-          const y2 = startY + toNode.y * nodeYGap;
-
-          const dx = x2 - x1;
-          const dy = y2 - y1;
-
-          const angle = Math.atan2(dy, dx);
-          const lineDx = Math.cos(angle) * linePadding;
-          const lineDy = Math.sin(angle) * linePadding;
-
-          // return a line
-          return (
-            <line
-              x1={x1 + lineDx}
-              y1={y1 + lineDy}
-              x2={x2 - lineDx}
-              y2={y2 - lineDy}
+            )}
+            <circle
+              key={`circle-${i}`}
+              cx={x}
+              cy={y}
+              r={nodeRadius}
+              fill="white"
               stroke="black"
               strokeWidth="3"
             />
-          );
-        })}
-      </svg>
+
+            <text
+              key={`label-${i}`}
+              x={x}
+              y={y}
+              fontFamily="Arial, sans-serif"
+              fontSize="22"
+              text-anchor="middle"
+              dominant-baseline="central"
+              fill="black"
+            >
+              {node.label}
+            </text>
+
+            <text
+              key={`branch-${i}`}
+              x={x + tagOffsetX}
+              y={y - tagOffsetY}
+              fontFamily="Arial, sans-serif"
+              fontSize="14"
+              fill="black"
+            >
+              branch: {node.branch}
+            </text>
+            <text
+              key={`clock-${i}`}
+              x={x + tagOffsetX}
+              y={y - tagOffsetY - tagSpacingY}
+              fontFamily="Arial, sans-serif"
+              fontSize="14"
+              fill="black"
+            >
+              clock: {node.clock}
+            </text>
+          </>
+        );
+      })}
+
+      {edges.map(({ from, to }) => {
+        const fromNode = nodesByLabel[from];
+        const toNode = nodesByLabel[to];
+
+        const x1 = startX + fromNode.x * nodeXGap;
+        const y1 = startY + fromNode.y * nodeYGap;
+
+        const x2 = startX + toNode.x * nodeXGap;
+        const y2 = startY + toNode.y * nodeYGap;
+
+        const dx = x2 - x1;
+        const dy = y2 - y1;
+
+        const angle = Math.atan2(dy, dx);
+        const lineDx = Math.cos(angle) * linePadding;
+        const lineDy = Math.sin(angle) * linePadding;
+
+        // return a line
+        return (
+          <line
+            x1={x1 + lineDx}
+            y1={y1 + lineDy}
+            x2={x2 - lineDx}
+            y2={y2 - lineDy}
+            stroke="black"
+            strokeWidth="3"
+          />
+        );
+      })}
     </>
   );
 };
